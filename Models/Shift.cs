5b15a48288b556ac63bad6224d9d310d3156aee6
Pachinoo <rpanzinskis@gmail.com>
2025-05-23 19:20:42 +0300

    Otrais push

--- Models/Shift.cs
+++ Models/Shift.cs
@@ -5,12 +5,12 @@ namespace EmployeeSchedulePlanner.Models
     public class Shift
     {
         public int Id { get; set; }
-        public int UserId { get; set; }  // Aizvietots EmployeeId
+        public int UserId { get; set; }  
         public DateTime ShiftDate { get; set; }
         public TimeSpan StartTime { get; set; }
         public TimeSpan EndTime { get; set; }
         public string Notes { get; set; }
 
-        public User User { get; set; }  // Aizvietots Employee
+        public User User { get; set; }
     }
 }
